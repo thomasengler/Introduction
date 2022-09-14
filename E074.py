9f28e749531e78ac18fc0afa5e1406f64646293e
Thomas Engler <thomas_engler@gmx.net>
2022-09-14 15:21:28 +0200

    Done

---
+++ E074.py
@@ -0,0 +1,35 @@
+"""
+▪ Define at least seven different variables of different types:
+ Integer
+ Float
+ String
+▪ Print out the corresponding type of them
+"""
+# int
+oven_temperature = 190
+turn_steak_in_grad = 180
+
+# string
+greetings = "Servus"
+steak_welldone = "well done"
+steak_med = "medium"
+steak_bloody = "bloddy"
+
+# float
+meat_conenstense = 125e12
+
+# bool
+fire_on = True
+oven_open = True
+oven_finished = False
+
+print(type(oven_temperature))
+print(type(turn_steak_in_grad))
+print(type(greetings))
+print(type(steak_welldone))
+print(type(steak_med))
+print(type(steak_bloody))
+print(type(meat_conenstense))
+print(type(fire_on))
+print(type(oven_open))
+print(type(oven_finished))
